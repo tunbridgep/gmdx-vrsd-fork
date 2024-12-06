tunbridgep/gmdx-vrsd-fork

commit 7306baf42cb292aef05163a9d56c77ce9db481cb

Hacking Overhaul. (#124)

_Classes/DeusEx/Classes/ComputerScreenSecurity.uc
@@ -75,6 +75,25 @@ event DestroyWindow()
       Removetimer(networkTimerID);
       networkTimerID = -1;
    }
+
+    //SARGE: Refresh rebooting duration when closing the window
+    if (winCameras[0].camera.bRebooting)
+        winCameras[0].camera.StartReboot(player);
+    if (winCameras[1].camera.bRebooting)
+        winCameras[1].camera.StartReboot(player);
+    if (winCameras[2].camera.bRebooting)
+        winCameras[2].camera.StartReboot(player);
+    if (winCameras[3].camera.bRebooting)
+        winCameras[3].camera.StartReboot(player);
+    
+    if (winCameras[0].turret.bRebooting)
+        winCameras[0].turret.StartReboot(player);
+    if (winCameras[1].turret.bRebooting)
+        winCameras[1].turret.StartReboot(player);
+    if (winCameras[2].turret.bRebooting)
+        winCameras[2].turret.StartReboot(player);
+    if (winCameras[3].turret.bRebooting)
+        winCameras[3].turret.StartReboot(player);
 }
 
 // -----------------------------------------------------------------------
@@ -105,6 +124,14 @@ function DoorRefreshTimer(int timerID, int invocations, int clientData)
 	winCameras[2].UpdateDoorStatus();
 }
 
+function Tick(float deltaTime)
+{
+    choiceWindows[0].UpdateText(winTerm.bHacked);
+    choiceWindows[1].UpdateText(winTerm.bHacked);
+    choiceWindows[2].UpdateText(winTerm.bHacked);
+    choiceWindows[3].UpdateText(winTerm.bHacked);
+}
+
 // ----------------------------------------------------------------------
 // CreateControls()
 // ----------------------------------------------------------------------
@@ -121,6 +148,7 @@ function CreateControls()
 	CreateHeaders();
 	CreatePanSlider();
 	CreateInfoWindow();
+    bTickEnabled = true;
 }
 
 // ----------------------------------------------------------------------
@@ -323,10 +351,7 @@ function ToggleCameraState(optional bool bCamIsActive, optional bool bCamWasActi
 
 		if (cam != None)
 		{
-            if (winTerm.bHacked)
-                player.ToggleCameraStateHacked(cam, compOwner);
-            else
-                player.ToggleCameraState(cam, compOwner);
+            player.ToggleCameraState(cam, compOwner, winTerm.bHacked);
             selectedCamera.UpdateCameraStatus();
 		}
 	}
@@ -351,7 +376,7 @@ function AllyCamera()
 		}
       if ((selectedCamera.turret != None) && (Player.Level.Netmode != NM_Standalone))
       {
-         player.SetTurretTrackMode(ComputerSecurity(selectedCamera.compOwner),selectedCamera.turret,false,true);
+         player.SetTurretTrackMode(ComputerSecurity(selectedCamera.compOwner),selectedCamera.turret,false,true,false);
          selectedCamera.UpdateTurretStatus();
          ComputerSecurityChoice_Turret(choiceWindows[3]).SetMPEnumState();
       }
@@ -402,10 +427,7 @@ function SetTurretState(bool bActive, bool bDisabled)
 {
 	if ((selectedCamera != None) && (selectedCamera.turret != None))
 	{
-        if (winTerm.bHacked)
-            player.SetTurretStateHacked(selectedCamera.turret,bActive,bDisabled);
-        else
-            player.SetTurretState(selectedCamera.turret,bActive,bDisabled);
+        player.SetTurretState(selectedCamera.turret,bActive,bDisabled,winTerm.bHacked);
         selectedCamera.UpdateTurretStatus();
 	}
 }
@@ -418,7 +440,7 @@ function SetTurretTrackMode(bool bTrackPlayers, bool bTrackPawns)
 {
 	if ((selectedCamera != None) && (selectedCamera.turret != None))
 	{
-      player.SetTurretTrackMode(ComputerSecurity(selectedCamera.compOwner),selectedCamera.turret,bTrackPlayers,bTrackPawns);
+      player.SetTurretTrackMode(ComputerSecurity(selectedCamera.compOwner),selectedCamera.turret,bTrackPlayers,bTrackPawns,winTerm.bHacked);
 		selectedCamera.UpdateTurretStatus();
 
       if ((selectedCamera.camera != None) && (Player.Level.Netmode != NM_Standalone))

System/DeusEx.int
@@ -2864,8 +2864,11 @@ SkillName="Athletics"
 Description="An agent with extreme physical dedication can develop the skills to climb tall heights and swim great distances.|n|nTotal skill points to master: 4725|n|nUNTRAINED: An agent can swim and climb onto short ledges.|n|nTRAINED: The swimming speed and stamina of an agent increases slightly, and an agent can climb onto moderate ledges.|n|nADVANCED: The swimming speed and stamina of an agent increases moderately, and an agent can climb onto tall ledges.|n|nMASTER: An agent moves like a dolphin underwater and climbs like a jaguar."
 
 [SkillComputer]
-SkillName="Hacking"
-Description="The covert manipulation of computers and security consoles.|n|nTotal skill points to master: 6000.|n|nUNTRAINED: An agent can use terminals to read bulletins and news. |n|nTRAINED: An agent can hack computers and security consoles of intermediate security systems, can shut security systems down for 2 minutes, and siphon up to half of an ATM's funds. |n|nADVANCED: An agent can hack computers featuring advanced defense systems and siphon all credits from ATMs, and achieves a moderate increase in detection time as well as a 2 minute increase in security shutdown time.|n|nMASTER: An agent is an elite hacker that no system can withstand, gaining the ability to spoof additional credits from ATMs and evading detection for long periods, including an additional 2 minute increase in security shutdown time."
+SkillName="Computers"
+Description="The covert manipulation of computers and security consoles.|n|nTotal skill points to master: 6000.|n|nUNTRAINED: An agent can use terminals to read bulletins and news. |n|nTRAINED: An agent can hack computers and security consoles of intermediate security systems and siphon up to half of an ATM's funds. |n|nADVANCED: An agent can hack computers featuring advanced defense systems and siphon all credits from ATMs, and achieves a moderate increase in detection time.|n|nMASTER: An agent is an elite hacker that no system can withstand, gaining the ability to spoof additional credits from ATMs and evading detection for long periods."
+strHardcore="HARDCORE MODE ADDITIONS:"
+strNewHackSystem="NEW HACKING SYSTEM ADDITIONS:"
+strDescExtra="Devices will reboot after a certain time, returning to their default state. Additionally, hacking the same terminal repeatedly will result in network detection, preventing future hacking attempts.|n|nReboot Times:|n2 minutes at TRAINED, plus 1 additional minute on ADVANCED and MASTER (4 Minutes Total)|n|nMaximum Successful Hacks:|n2 plus 1 additional hack on ADVANCED and MASTER (4 Total)"
 
 [SkillTech]
 SkillName="Electronics"

_Classes/DeusEx/Classes/AugmentationDisplayWindow.uc
@@ -1484,11 +1484,13 @@ function DrawTargetAugmentation(GC gc)
 	DeusExRootWindow(player.rootWindow).hud.hitmarker.SetCrosshairColor(crossColor);
 }
 
+//SARGE: Get the text for rebooting cameras/turrets
 function string GetHackDisabledText(Actor target,bool TargetingDisplay)
 {
     local SecurityCamera cam;
     local AutoTurret turr;
-    local string str;
+    local string str, strT;
+    local int amt, min, sec;
 
     if (target.IsA('AutoTurretGun'))
         turr = AutoTurret(target.Owner);
@@ -1497,10 +1499,45 @@ function string GetHackDisabledText(Actor target,bool TargetingDisplay)
     
     cam = SecurityCamera(target);
 
+
+
     if (turr != None && turr.bRebooting)
-        str = Sprintf(msgReboot,int(turr.disableTime - player.saveTime));
+        amt = int(turr.disableTime - player.saveTime);
     else if (cam != None && cam.bRebooting)
-        str = Sprintf(msgReboot,int(cam.disableTime - player.saveTime));
+        amt = int(cam.disableTime - player.saveTime);
+
+    //ZAP!
+    if (amt == 0)
+        return "";
+    /*
+    else if (turr != None && turr.bConfused && turr.bRebooting)
+        return Sprintf(msgReboot,"-:--");
+    else if (cam != None && cam.bConfused && cam.bRebooting)
+        return Sprintf(msgReboot,"-:--");
+    */
+
+    //Now format it as minutes and seconds
+    min = amt / 60;
+    sec = amt % 60;
+
+    //minute
+    if (min > 0)
+        strT = strT $ min;
+    else
+        strT = strT $ "0";
+
+    //divider
+    strT = strT $ ":";
+
+    //seconds
+    if (sec > 0 && sec < 10)
+        strT = strT $ "0" $ sec;
+    else if (sec > 0)
+        strT = strT $ sec;
+    else
+        strT = strT $ "00";
+        
+    str = Sprintf(msgReboot,strT);
 
     //If using the targeting aug, we need to format it
     if (TargetingDisplay && str != "")
@@ -1932,7 +1969,7 @@ defaultproperties
      msgIRAmpActive="IRAmp Active"
      msgNoImage="Image Not Available"
      msgDisabled="Disabled"
-     msgReboot="Rebooting in %ds"
+     msgReboot="Rebooting in %s"
      SpottedTeamString="You have spotted a teammate!"
      YouArePoisonedString="You have been poisoned!"
      YouAreBurnedString="You are burning!"

_Classes/DeusEx/Classes/AutoTurret.uc
@@ -42,7 +42,8 @@ var Pawn savedTarget;
 
 //Sarge: Hacking disable time
 var float disableTime;                    //Sarge: timer before we are enabled again after hacking.
-const disableTimeMult = 120.0;            //Sarge: Our hacking skill is multiplied by this to give total disable time
+var float disableTimeBase;                //Sarge: Our hacking skill is multiplied by this to give total disable time
+var float disableTimeMult;                //Sarge: Our hacking skill is multiplied by this to give total disable time
 var bool bRebooting;                      //This will be set when the turret is hacked, to control rebooting
 
 // networking replication
@@ -69,8 +70,6 @@ function Trigger(Actor Other, Pawn Instigator)
 // if we are untriggered, turn us off
 function UnTrigger(Actor Other, Pawn Instigator)
 {
-    bRebooting = false;
-
 	if (bActive)
 	{
 		bActive = False;
@@ -201,6 +200,16 @@ function Actor AcquireMultiplayerTarget()
 	return curtarget;
 }
 
+//SARGE: Telling this turret that it should start rebooting right now
+function StartReboot(DeusExPlayer player)
+{
+    if (player.bHardCoreMode || player.bHackLockouts)
+    {
+        bRebooting = true;
+        disableTime = player.saveTime + disableTimeBase + (disableTimeMult * MAX(0,player.SkillSystem.GetSkillLevel(class'SkillComputer') - 1));
+    }
+}
+
 function Tick(float deltaTime)
 {
 	local Pawn pawn;
@@ -226,8 +235,6 @@ function Tick(float deltaTime)
     
     if (bRebooting && !bConfused)
     {
-        bDisabled = True;
-
         if (remainingTime <= 0)
         {
             if (bDisabled && gun.hackStrength != 0.0)
@@ -283,6 +290,10 @@ function Tick(float deltaTime)
 	if (bConfused)
 	{
 		confusionTimer += deltaTime;
+        
+        //SARGE: Confusing pauses hacking reboot time
+        if (bRebooting)
+            disableTime += deltaTime;
 
 		// pick a random facing
 		if (confusionTimer % 0.25 > 0.2)
@@ -935,4 +946,6 @@ defaultproperties
      Mass=50.000000
      Buoyancy=10.000000
      bVisionImportant=True
+     disableTimeBase=120.0;
+     disableTimeMult=60.0;
 }

_Classes/DeusEx/Classes/ComputerCameraUIChoice.uc
@@ -7,6 +7,7 @@ class ComputerCameraUIChoice extends MenuUIChoiceEnum
 
 var ComputerSecurityCameraWindow winCamera;
 var ComputerScreenSecurity       securityWindow;
+var localized string strRebooting;
 
 // ----------------------------------------------------------------------
 // SetCameraView()
@@ -36,6 +37,11 @@ function DisableChoice()
 	btnInfo.DisableWindow();
 }
 
+//We need to update the text for our button based on whether we are turning it off or simply making it reboot
+function UpdateText(bool bHacking)
+{
+}
+
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 
@@ -43,4 +49,5 @@ defaultproperties
 {
      defaultInfoWidth=113
      defaultInfoPosX=154
+     strRebooting="Reboot in %d"
 }

_Classes/DeusEx/Classes/ComputerScreenDisabled.uc
@@ -5,6 +5,7 @@ class ComputerScreenDisabled extends ComputerUIWindow;
 
 var MenuUILabelWindow        winLoginInfo;
 var MenuUIActionButtonWindow btnClose;
+var MenuUIActionButtonWindow btnLogin;
 
 var localized String ButtonLabelClose;
 var localized String LoginInfoText;
@@ -19,6 +20,7 @@ function CreateControls()
 	Super.CreateControls();
 
 	btnClose = winButtonBar.AddButton(ButtonLabelClose, HALIGN_Right);
+	btnLogin = winButtonBar.AddButton(ButtonLabelLogin, HALIGN_Right);
 
 	CreateLoginInfoWindow();
 
@@ -69,6 +71,10 @@ function bool ButtonActivated( Window buttonPressed )
 		case btnClose:
 			CloseScreen("EXIT");
 			break;
+		
+        case btnLogin:
+			CloseScreen("LOGOUT");
+			break;
 
 		default:
 			bHandled = False;
@@ -87,7 +93,7 @@ function bool ButtonActivated( Window buttonPressed )
 defaultproperties
 {
      ButtonLabelClose="Close"
-     LoginInfoText="This terminal has been locked.|n|nReason: Network Threat Analysis policy has locked this terminal due to suspicious activity.|n|nFor more information, please contact your network administrator.|n"
+     LoginInfoText="Due to suspicious network activity, this terminal has been placed under quarantine by security policy. Special network access functions have been restricted.|nFor more information, please contact your network administrator.|n"
      StatusText="SECUR//GLOBAL//9571.2256"
      Title="Security Notice"
      ClientWidth=403

_Classes/DeusEx/Classes/ComputerSecurityChoice_Camera.uc
@@ -53,8 +53,8 @@ function SetCameraView(ComputerSecurityCameraWindow newCamera)
 
 function bool ButtonActivated( Window buttonPressed )
 {
-	Super.ButtonActivated(buttonPressed);
 	securityWindow.ToggleCameraState();
+	Super.ButtonActivated(buttonPressed);
 	return True;
 }
 
@@ -64,9 +64,21 @@ function bool ButtonActivated( Window buttonPressed )
 
 function bool ButtonActivatedRight( Window buttonPressed )
 {
-	Super.ButtonActivated(buttonPressed);
 	securityWindow.ToggleCameraState();
+	Super.ButtonActivated(buttonPressed);
 	return True;
+}
+
+//SARGE: When hacked, replace Off with the Rebooting text
+function UpdateText(bool bHacking)
+{
+    local int disableTime;
+    if (winCamera.camera.bRebooting && !winCamera.camera.bActive && !bHacking)
+    {
+        SetValue(0); //If not hacked, Next choice will be Off.
+        disableTime = winCamera.camera.disableTime - player.saveTime;
+        btnInfo.SetButtonText(sprintf(strRebooting,disableTime));
+    }
 }
 
 // ----------------------------------------------------------------------

_Classes/DeusEx/Classes/ComputerSecurityChoice_Turret.uc
@@ -152,6 +152,21 @@ function SetValue(int newValue)                                                 
 	UpdateInfoButton();
 }
 
+//SARGE: When hacked, replace Off with the Rebooting text
+//Since we have multiple states here, and not much room on the button,
+//we will only show the Rebooting text if we log-in normally, and set the next state to Off
+function UpdateText(bool bHacking)
+{
+    local int disableTime;
+    if (winCamera.turret.bRebooting && !bHacking)
+    {
+        SetValue(1); //Next value goes to either Enemies or Bypassed
+        disableTime = winCamera.turret.disableTime - player.saveTime;
+        btnInfo.SetButtonText(sprintf(strRebooting,disableTime));
+    }
+}
+
+
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 

commit 3d1212cc2e9e54e20f89f84640ff50a52ead0728

Fixes crashing issue introduced in #124

_Classes/DeusEx/Classes/ComputerScreenSecurity.uc
@@ -55,6 +55,33 @@ function InitWindow()
 }
 
 // ----------------------------------------------------------------------
+// SARGE: RefreshRebootDuration()
+//
+// Refresh rebooting duration when closing the window
+// ----------------------------------------------------------------------
+
+function RefreshRebootDuration()
+{
+    if (winCameras[0].camera != None && winCameras[0].camera.bRebooting)
+        winCameras[0].camera.StartReboot(player);
+    if (winCameras[1].camera != None && winCameras[1].camera.bRebooting)
+        winCameras[1].camera.StartReboot(player);
+    if (winCameras[2].camera != None && winCameras[2].camera.bRebooting)
+        winCameras[2].camera.StartReboot(player);
+    if (winCameras[3].camera != None && winCameras[3].camera.bRebooting)
+        winCameras[3].camera.StartReboot(player);
+    
+    if (winCameras[0].turret != None && winCameras[0].turret.bRebooting)
+        winCameras[0].turret.StartReboot(player);
+    if (winCameras[1].turret != None && winCameras[1].turret.bRebooting)
+        winCameras[1].turret.StartReboot(player);
+    if (winCameras[2].turret != None && winCameras[2].turret.bRebooting)
+        winCameras[2].turret.StartReboot(player);
+    if (winCameras[3].turret != None && winCameras[3].turret.bRebooting)
+        winCameras[3].turret.StartReboot(player);
+}
+
+// ----------------------------------------------------------------------
 // DestroyWindow()
 //
 // Destroys the Window
@@ -62,7 +89,8 @@ function InitWindow()
 
 event DestroyWindow()
 {
-	Super.DestroyWindow();
+	
+    Super.DestroyWindow();
 
 	if (doorTimerID != -1)
 	{
@@ -75,25 +103,6 @@ event DestroyWindow()
       Removetimer(networkTimerID);
       networkTimerID = -1;
    }
-
-    //SARGE: Refresh rebooting duration when closing the window
-    if (winCameras[0].camera.bRebooting)
-        winCameras[0].camera.StartReboot(player);
-    if (winCameras[1].camera.bRebooting)
-        winCameras[1].camera.StartReboot(player);
-    if (winCameras[2].camera.bRebooting)
-        winCameras[2].camera.StartReboot(player);
-    if (winCameras[3].camera.bRebooting)
-        winCameras[3].camera.StartReboot(player);
-    
-    if (winCameras[0].turret.bRebooting)
-        winCameras[0].turret.StartReboot(player);
-    if (winCameras[1].turret.bRebooting)
-        winCameras[1].turret.StartReboot(player);
-    if (winCameras[2].turret.bRebooting)
-        winCameras[2].turret.StartReboot(player);
-    if (winCameras[3].turret.bRebooting)
-        winCameras[3].turret.StartReboot(player);
 }
 
 // -----------------------------------------------------------------------
@@ -115,6 +124,7 @@ function NetworkRefreshTimer(int timerID, int invocations, int clientData)
 // DoorRefreshTimer()
 //
 // Timer function to refresh the door status text
+// SARGE: Now updates the turrets and cameras as well
 // ----------------------------------------------------------------------
 
 function DoorRefreshTimer(int timerID, int invocations, int clientData)
@@ -122,10 +132,7 @@ function DoorRefreshTimer(int timerID, int invocations, int clientData)
 	winCameras[0].UpdateDoorStatus();
 	winCameras[1].UpdateDoorStatus();
 	winCameras[2].UpdateDoorStatus();
-}
-
-function Tick(float deltaTime)
-{
+    
     choiceWindows[0].UpdateText(winTerm.bHacked);
     choiceWindows[1].UpdateText(winTerm.bHacked);
     choiceWindows[2].UpdateText(winTerm.bHacked);
@@ -587,6 +594,7 @@ function bool ButtonActivated( Window buttonPressed )
 				break;
 
 			case btnLogout:
+                RefreshRebootDuration();
 				CloseScreen("LOGOUT");
 				break;
 