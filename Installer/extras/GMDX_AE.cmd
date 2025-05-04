cce7b834d07fd1315b24c44bfb6b79382dae41fb
Paul Tunbridge <tunbridgep@gmail.com>
2025-05-04 23:38:08 +1000

    Updated Batch to pass on any arguments given, and added some more documentation.

--- Installer/extras/GMDX_AE.cmd
+++ Installer/extras/GMDX_AE.cmd
@@ -1,1 +1,3 @@
-DeusEx.exe INI="..\GMDX_AE\System\DeusEx.ini" USERINI="..\GMDX_AE\System\User.ini"
+::Use BAT to EXE converter to make GMDX_AE.exe
+::https://github.com/l-urk/Bat-To-Exe-Converter-64-Bit/releases
+DeusEx.exe INI="..\GMDX_AE\System\DeusEx.ini" USERINI="..\GMDX_AE\System\User.ini" %*
